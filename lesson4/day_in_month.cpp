a414d1835892fe2c01df54662c3cbbd56ccf6def
Bryukvina Darja <100697083+Bryukvina@users.noreply.github.com>
2022-10-08 06:42:52 +0300

    Update day_in_month.cpp

--- lesson4/day_in_month.cpp
+++ lesson4/day_in_month.cpp
@@ -27,18 +27,21 @@ int main(){
     
     switch (month)
     {
-        case MONTHS::Jan: std::cout << "31" << std::endl; break;
-        case MONTHS::Feb: std::cout << "28" << std::endl; break;
-        case MONTHS::Mar: std::cout << "31" << std::endl; break;
-        case MONTHS::Apr: std::cout << "30" << std::endl; break;
-        case MONTHS::May: std::cout << "31" << std::endl; break;
-        case MONTHS::Jun: std::cout << "30" << std::endl; break;
-        case MONTHS::Jul: std::cout << "31" << std::endl; break;
-        case MONTHS::Aug: std::cout << "31" << std::endl; break;
-        case MONTHS::Sep: std::cout << "30" << std::endl; break;
-        case MONTHS::Oct: std::cout << "31" << std::endl; break;
-        case MONTHS::Nov: std::cout << "30" << std::endl; break;
+        case MONTHS::Jan:;
+        case MONTHS::Mar:;
+        case MONTHS::May:;
+        case MONTHS::Jul:;
+        case MONTHS::Aug:;
+        case MONTHS::Oct:;
         case MONTHS::Dec: std::cout << "31" << std::endl; break;
+
+        case MONTHS::Feb: std::cout << "28" << std::endl; break;
+
+        case MONTHS::Apr:;
+        case MONTHS::Jun:;
+        case MONTHS::Sep:;
+        case MONTHS::Nov: std::cout << "30" << std::endl; break;
+
     }
     
     return 0;
