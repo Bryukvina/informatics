40f160095bf726176d84f60d19efc96b232ed2ab
Bryukvina Darja <100697083+Bryukvina@users.noreply.github.com>
2022-11-10 20:31:58 +0300

    Create main.cpp

---
+++ Lesson_6/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+#include "frac.hpp"
+#include "frac.cpp"
+
+int main(){
+    Fraction f1, f2;
+    double c = 2.5;
+    int k = 3;
+    std::cin >> f1;
+    f2 = Fraction(c);
+    std::cout << f2;
+    std::cout << f1 * f2 << std::endl;
+    std::cout << k + f2 << std::endl;
+    std::cout << f1 + f2 << std::endl;
+    std::cout << f1 - f2 << std::endl;
+    std::cout << f1 / f2 << std::endl;
+
+    f1 += 2;
+    std::cout << f1 << std::endl;
+    f2 -= f1;
+    std::cout << f2 << std::endl;
+    std::cout << "f1 == f2?" << (f1 == f2) << std::endl;
+};
