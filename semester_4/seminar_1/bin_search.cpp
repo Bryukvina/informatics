99176f835bfa0e8968503108b9c8b9eac0fd52c0
Bryukvina Darja <100697083+Bryukvina@users.noreply.github.com>
2023-02-26 20:51:35 +0300

    Update bin_search.cpp

--- semester_4/seminar_1/bin_search.cpp
+++ semester_4/seminar_1/bin_search.cpp
@@ -1,7 +1,10 @@
+#include <iostream>
 #include <cmath>
 #include <vector>
 
-auto binsearch(std::vector arr, int key){
+
+//RA-iterators
+int binsearch_RA(std::vector arr, int key){
     auto it_beg = arr.begin();
     auto it_end = arr.end();
     auto it_mid = it_beg + (it_end - it_beg) / 2;
@@ -12,3 +15,34 @@ auto binsearch(std::vector arr, int key){
         it_mid = it_beg + (it_end - it_beg) / 2;
     }
  return it_mid;
+
+//forward-iterator
+ int binsearch_for(std::vector arr, int key){
+    auto it_beg = arr.begin();
+    auto it_end = arr.end();
+    int n = arr.size() / 2;
+    int i;
+    while (n > 1 && *it_sup != key) {
+        auto it_sup = it_beg;
+        for (i = 0, n, i++){++it_sup};
+        if (*it_sup < key) {auto it_beg = it_sup};
+        n /= 2;
+    }
+ return it_sup;
+
+//bidirectional -iterators
+int binsearch_for(std::vector arr, int key){
+    auto it_beg = arr.begin();
+    auto it_end = arr.end();
+    int n = arr.size() / 2;
+    int i;
+    while (it_beg < it_end && *it_end != key) {
+        if (*it_mid < key){
+            for (i = 0, n, i++){++it_beg};
+        };
+        else {
+            for (i = 0, n, i++){--it_end};
+        };
+        n /= 2;
+    }
+ return it_end;
