f20300f9270a49d3f167d61a4225a66dadf75645
Bryukvina Darja <100697083+Bryukvina@users.noreply.github.com>
2023-02-26 17:21:07 +0300

    Update spec_func.cpp

--- semester_4/seminar_1/spec_func.cpp
+++ semester_4/seminar_1/spec_func.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <chrono>
 
 class MyClass{
     public:
@@ -14,8 +15,12 @@ class MyClass{
             size = object.size;
             arr = object.arr;
             vect = object.vect;
+            const auto end_time = std::chrono::steady_clock::now();
+            const auto dur = end_time - start_time_;
+            std::cout << "copy: ";
+            std::cout << "operation time" << ": " << std::chrono::duration_cast<std::chrono::milliseconds>(dur).count() << " ms" << std::endl;
         };
-  
+
         MyClass operator=(const MyClass& object){
             if (this == &object) return *this;
             size = object.size;
@@ -23,15 +28,17 @@ class MyClass{
             vect = object.vect;
             return *this
         };
-  
-        MyClass(MyClass&& Object) noexcept {
-            size = object.size;
+        MyClass(MyClass&& Object) noexcept 
+        {size = object.size;
             arr = object.arr;
             vect = object.vect;
             object.size = 0;
             delete object.arr;
             object.vect = 0;
+            std::cout << "move: ";
+            std::cout << "operation time" << ": " << std::chrono::duration_cast<std::chrono::milliseconds>(dur).count() << " ms" << std::endl;
         };
+            };
         
         MyClass& operator=(const MyClass& object){
             if (this == &object) return *this;
