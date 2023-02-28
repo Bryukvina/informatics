81eeb291c2a1b0396b6021691b56bc8d9b7302f6
Bryukvina Darja <100697083+Bryukvina@users.noreply.github.com>
2023-02-28 15:19:54 +0300

    Update bin_search.cpp

--- semester_4/seminar_1/bin_search.cpp
+++ semester_4/seminar_1/bin_search.cpp
@@ -1,39 +1,73 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <interator>
+#include <chrono>
 
-
-//RA-iterators
-int binsearch_RA(std::vector arr, int key){
-    auto it_beg = arr.begin();
-    auto it_end = arr.end();
-    auto it_mid = it_beg + (it_end - it_beg) / 2;
-    while (it_mid != it_end *it_mid != key) {
-        if (*it_mid > key) it_end = it_mid;
-        else it_beg = it_mid + 1;
-
-        it_mid = it_beg + (it_end - it_beg) / 2;
+class LogDuration {
+public:
+    LogDuration(std::string id)
+        : id_(std::move(id)) {
     }
- return it_mid;
 
-//forward-iterator
- int binsearch_for(std::vector arr, int key){
-    auto it_beg = arr.begin();
-    auto it_end = arr.end();
+    ~LogDuration() {
+        const auto end_time = std::chrono::steady_clock::now();
+        const auto dur = end_time - start_time_;
+        std::cout << id_ << ": ";
+        std::cout << "operation time"
+                  << ": " << std::chrono::duration_cast<std::chrono::milliseconds>(dur).count()
+                  << " ms" << std::endl;
+    }
+
+private:
+    const std::string id_;
+    const std::chrono::steady_clock::time_point start_time_ = std::chrono::steady_clock::now();
+};
+
+
+
+
+template <typename value, std::forward_interator It>;
+
+value binsearch(std::vector<value> arr, value key){
+    LogDuration ld("identifier");
+    It it_beg = arr.begin();
+    It it_end = arr.end();
     int n = arr.size() / 2;
     int i;
     while (n > 1 && *it_sup != key) {
-        auto it_sup = it_beg;
+        It it_sup = it_beg;
         for (i = 0, n, i++){++it_sup};
-        if (*it_sup < key) {auto it_beg = it_sup};
+        if (*it_sup < key) {It it_beg = it_sup};
         n /= 2;
     }
- return it_sup;
+    return it_sup;
+};
 
-//bidirectional -iterators
+
+
+template <typename value, std::random_access_iterator It>;
+
+value binsearch(std::vector<value> arr, value key){
+    LogDuration ld("identifier");
+    It it_beg = arr.begin();
+    It it_end = arr.end();
+    It it_mid = it_beg + (it_end - it_beg) / 2;
+    while (it_mid != it_end *it_mid != key) {
+        if (*it_mid > key) it_end = it_mid;
+        else it_beg = it_mid + 1;
+        it_mid = it_beg + (it_end - it_beg) / 2;
+    }
+    return it_mid;
+};
+
+
+template <typename value, std::bidirectional_iterator It>;
+
 int binsearch_for(std::vector arr, int key){
-    auto it_beg = arr.begin();
-    auto it_end = arr.end();
+    LogDuration ld("identifier");
+    It it_beg = arr.begin();
+    It it_end = arr.end();
     int n = arr.size() / 2;
     int i;
     while (it_beg < it_end && *it_end != key) {
@@ -45,4 +79,5 @@ int binsearch_for(std::vector arr, int key){
         };
         n /= 2;
     }
- return it_end;
+    return it_end;
+}
