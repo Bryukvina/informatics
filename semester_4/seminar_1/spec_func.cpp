1add27541253b42f55c98446b45a9aa19fafc5cf
Bryukvina Darja <100697083+Bryukvina@users.noreply.github.com>
2023-02-28 16:00:22 +0300

    Update spec_func.cpp

--- semester_4/seminar_1/spec_func.cpp
+++ semester_4/seminar_1/spec_func.cpp
@@ -3,6 +3,28 @@
 #include <vector>
 #include <chrono>
 
+class LogDuration {
+public:
+    LogDuration(std::string id)
+        : id_(std::move(id)) {
+    }
+
+    ~LogDuration() {
+        const auto end_time = std::chrono::steady_clock::now();
+        const auto dur = end_time - start_time_;
+        std::cout << id_ << ": ";
+        std::cout << "operation time"
+                  << ": " << std::chrono::duration_cast<std::chrono::milliseconds>(dur).count()
+                  << " ms" << std::endl;
+    }
+
+private:
+    const std::string id_;
+    const std::chrono::steady_clock::time_point start_time_ = std::chrono::steady_clock::now();
+};
+
+
+
 class MyClass{
     public:
         int size=10;
@@ -12,44 +34,46 @@ class MyClass{
         MyClass() = default;
 
         MyClass(const MyClass &object){
-            size = object.size;
-            arr = object.arr;
-            vect = object.vect;
-            const auto end_time = std::chrono::steady_clock::now();
-            const auto dur = end_time - start_time_;
-            std::cout << "copy: ";
-            std::cout << "operation time" << ": " << std::chrono::duration_cast<std::chrono::milliseconds>(dur).count() << " ms" << std::endl;
+            LogDuration ld("copy:");
+            size = std::copy(object.size);
+            arr = std::copy(object.arr);
+            vect = std::copy(object.vect);
         };
 
         MyClass operator=(const MyClass& object){
-            if (this == &object) return *this;
-            size = object.size;
-            arr = object.arr;
-            vect = object.vect;
-            return *this
+            LogDuration ld("copy operator:");
+            size = std::copy(object.size);
+            arr = std::copy(object.arr);
+            vect = std::copy(object.vect);
+            return *this;
         };
-        MyClass(MyClass&& Object) noexcept 
-        {size = object.size;
-            arr = object.arr;
-            vect = object.vect;
-            object.size = 0;
-            delete object.arr;
-            object.vect = 0;
-            std::cout << "move: ";
-            std::cout << "operation time" << ": " << std::chrono::duration_cast<std::chrono::milliseconds>(dur).count() << " ms" << std::endl;
+
+        MyClass(MyClass&& Object) noexcept {
+            LogDuration ld("move");
+            size = std::move(object.size);
+            arr = std::move(Object.arr);
+            size = std::move(Object.size);
+            vect = std::move(Object.vect);
         };
-            };
         
-        MyClass& operator=(const MyClass& object){
-            if (this == &object) return *this;
-            size = object.size;
-            arr = object.arr;
-            vect = object.vect;
-            object.size = 0;
-            delete object.arr;
-            object.vect = 0;
-            return *this
+        MyClass& operator=(const MyClass&& object){
+            LogDuration ld("move operator:");
+            arr = std::move(Object.arr);
+            size = std::move(Object.size);
+            vect = std::move(Object.vect);
+            return *this;
         };
 
         ~MyClass() = default; 
+};
+
+
+int main(){
+    MyClass cl;
+    std::vector<int> vect = {1, 2, 3}
+    cl.vect = vect;
+    
+    
+    
+    return 0;
 };
