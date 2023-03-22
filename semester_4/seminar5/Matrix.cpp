e90a8c0eb865884d780ad4d4793b5db916e7bf2b
Bryukvina Darja <100697083+Bryukvina@users.noreply.github.com>
2023-03-22 08:00:11 +0300

    Create Matrix.cpp

---
+++ semester_4/seminar5/Matrix.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <cassert> 
+
+template <typename T>
+class MATRIX
+{
+private:
+  T** Matrix; // матрица
+  int m; // строки
+  int n; // столбцы
+
+public:
+  MATRIX()
+  {
+    n = m = 0;
+    Matrix = nullptr;
+  };
+
+  MATRIX(int _m, int _n)
+  {
+    m = _m;
+    n = _n;
+    Matrix = (T**) new T*[m];
+    for (int i = 0; i < m; i++){Matrix[i] = (T*)new T[n]};
+    for (int i = 0; i < m; i++){
+      for (int j = 0; j < n; j++){Matrix[i][j] = 0};
+    };
+  };
+
+  MATRIX(const MATRIX& _M)
+  {
+    m = _M.m;
+    n = _M.n;
+    Matrix = (T**) new T*[m];
+    for (int i = 0; i < m; i++){Matrix[i] = (T*) new T[n]};
+    for (int i = 0; i < m; i++){
+      for (int j = 0; j < n; j++){Matrix[i][j] = _M.Matrix[i][j]};
+    };
+  };
+
+  MATRIX operator=(const MATRIX& M)
+  {
+    if (n > 0)
+    {
+      for (int i = 0; i < m; i++)
+        delete[] Matrix[i];
+    }
+    if (m > 0)
+    {
+      delete[] M;
+    }
+    m = M.m;
+    n = M.n;
+    Matrix = (T**) new T*[m];
+    for (int i = 0; i < m; i++){Matrix[i] = (T*) new T[n]};
+    for (int i = 0; i < m; i++){
+      for (int j = 0; j < n; j++){Matrix[i][j] = M.Matrix[i][j]};
+    };
+    return *this;
+  };
+
+  ~MATRIX()
+  {
+    if (n > 0)
+    {
+      for (int i = 0; i < m; i++)
+        delete[] Matrix[i];
+    }
+    if (m > 0)
+      delete[] Matrix;
+  };
+
+  T GetMij(int i, int j)
+  {
+    assert(m > i && n > j );
+    return Matrix[i][j];
+  };
+
+  void SetMij(int i, int j, T value)
+  {
+    assert(m > i && n > j );
+    Matrix[i][j] = value;
+    return
+  };
+
+  MATRIX Sum(MATRIX M1, MATRIX M2){
+    assert((M1.n == M2.n) && (M1.m == M2.m));
+    MATRIX res(M1.m, M1.n);
+    for (int i = 0; i < m; i++){
+      for (int j = 0; j < n; j++)
+        {res.Matrix[i][j] = M1.Matrix[i][j] + M2.Matrix[i][j];};
+    };
+    return res;
+  };
+
+  MATRIX operator+(const MATRIX& M)
+    {
+      assert ((M.n == this->n) && (M.m == this->m));
+      if (this->n > 0)
+      {
+        for (int i = 0; i < m; i++)
+          delete[] this->Matrix[i];
+      }
+      if (this->m > 0)
+      {
+        delete[] M;
+      }
+      for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){this->Matrix[i][j] += M.Matrix[i][j]};
+      };
+      return *this;
+    };
+
+
+  MATRIX dif(MATRIX M1, MATRIX M2){
+    assert((M1.n == M2.n) && (M1.m == M2.m));
+    MATRIX res(M1.m, M1.n);
+    for (int i = 0; i < m; i++){
+      for (int j = 0; j < n; j++)
+        {res.Matrix[i][j] = M1.Matrix[i][j] + M2.Matrix[i][j];};
+    };
+    return res;
+  };
+
+  MATRIX operator-(const MATRIX& M)
+    {
+      assert ((M.n == this->n) && (M.m == this->m));
+      if (this->n > 0)
+      {
+        for (int i = 0; i < m; i++)
+          delete[] this->Matrix[i];
+      }
+      if (this->m > 0)
+      {
+        delete[] M;
+      }
+      for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){this->Matrix[i][j] -= M.Matrix[i][j]};
+      };
+      return *this;
+    };
+};
+
+
+void Print(const char* ObjName)
+{
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++){
+      std::cout << Matrix[i][j] << " ";
+      std::cout << endl;
+    };
+  };
+};
