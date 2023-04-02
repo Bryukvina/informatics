f024dc3fa2f00b5146c096660db9db250c86b575
Bryukvina Darja <100697083+Bryukvina@users.noreply.github.com>
2023-04-02 10:44:57 +0300

    Update Iteration_tuple.cpp

--- semester_4/seminar_2/Iteration_tuple.cpp
+++ semester_4/seminar_2/Iteration_tuple.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
-#include <iterator>
 #include <tuple>
+#include <iostream>
+#include <utility>
 
 template <typename T>
 void El(const T& x) {
@@ -8,13 +8,13 @@ void El(const T& x) {
 };
 
 template <typename Tup, std::size_t... list_s>
-void Iterate_Tuple(const Tup& tp) {
+void Iterate_Tuple(const Tup& tp, std::index_sequence<list_s...>) {
     (El(std::get<list_s>(tp)), ...);
 }
 
 /*Вызов функции*/
 int main(){
-    std::tuple<int> tp = {1, 2, 3, 4, 8, 9};
+    std::tuple<int, int, int, int, int> tp = {1, 2, 3, 4, 8};
     Iterate_Tuple(tp, std::make_index_sequence<std::tuple_size<decltype(tp)>::value>{});
     return 0;
 };
